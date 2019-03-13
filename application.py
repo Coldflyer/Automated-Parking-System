b14aed4c23ef10f5e6e8552e70e5afdf5949aa24
Cameron Lanata <cam@Camerons-MacBook-Pro.local>
2019-03-13 15:34:43 -0400

    Updated backend for demo

--- application.py
+++ application.py
@@ -7,6 +7,7 @@ Author: Scott Rodkey - rodkeyscott@gmail.com
 Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
 '''
 
+from flask import Flask, render_template, request, redirect, url_for
 from flask import Flask, render_template, request
 from application import db
 from application.models import Customer, Reservation
@@ -28,28 +29,23 @@ def home():
 
 @application.route('/checkin', methods=['GET', 'POST'])
 def checkin():
-    form1 = EnterFirstName(request.form)
-    form2 = EnterLastName(request.form)
-    form3 = EnterVehicleInfo(request.form)
-    form4 = EnterEstimatedDuration(request.form)
-
-    if request.method == 'POST' and form1.validate() and form2.validate() and form3.validate() and form4.validate():
-        data_entered = Customer(notes=form1.dbNotes.data)
+    if request.method == 'POST':
+        data_entered = Customer(fn=request.form['firstname'], ln=request.form['lastname'],
+                                vInfo=request.form['vInfo'], dur=request.form['duration'])
         try:
             db.session.add(data_entered)
             db.session.commit()
             db.session.close()
         except:
             db.session.rollback()
-        return render_template('parking.html', notes=form1.dbNotes.data)
+        return render_template('parking.html')
 
-    return render_template('checkin.html', form1=form1, form2=form2, form3=form3, form4=form4)
+    return render_template('checkin.html',)
 
 
 @application.route('/parking', methods=['GET', 'POST'])
 def parking():
     return render_template('parking.html')
-
 
 
 if __name__ == '__main__':
